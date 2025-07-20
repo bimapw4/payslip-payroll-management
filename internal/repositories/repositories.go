0a1beba5c25584c2d52a1658c5a07cd155159e8b
bimapw4 <bimapw4@gmail.com>
2025-07-21 00:01:30 +0700

    [attendance] repo - register attendance repo

--- internal/repositories/repositories.go
+++ internal/repositories/repositories.go
@@ -1,17 +1,20 @@
 package repositories
 
 import (
+	"payslips/internal/repositories/attendance"
 	"payslips/internal/repositories/users"
 
 	"github.com/jmoiron/sqlx"
 )
 
 type Repository struct {
-	Users users.Users
+	Users      users.Users
+	Attendance attendance.Attendance
 }
 
 func NewRepository(db *sqlx.DB) Repository {
 	return Repository{
-		Users: users.NewRepo(db),
+		Users:      users.NewRepo(db),
+		Attendance: attendance.NewRepo(db),
 	}
 }
