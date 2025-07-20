be059433772f6d2aed1e21abf7f1ecaaa48ff342
bimapw4 <bimapw4@gmail.com>
2025-07-20 21:32:37 +0700

    [business] auth - add authentication business

---
+++ internal/business/auth/auth.go
@@ -0,0 +1,65 @@
+package auth
+
+import (
+	"context"
+	"payslips/internal/common"
+	"payslips/internal/entity"
+	"payslips/internal/presentations"
+	"payslips/internal/repositories"
+
+	"strings"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Contract interface {
+	Authorization(ctx context.Context, payload entity.Authorization) (*presentations.AuthorizationResp, error)
+}
+
+type business struct {
+	repo *repositories.Repository
+	jwt  common.JwtCode
+}
+
+func NewBusiness(repo *repositories.Repository) Contract {
+	return &business{
+		repo: repo,
+	}
+}
+
+func (b *business) Authorization(ctx context.Context, payload entity.Authorization) (*presentations.AuthorizationResp, error) {
+
+	var (
+		users *presentations.Users
+		err   error
+	)
+
+	if payload.Type == "authorization" {
+		users, err = b.repo.Users.GetUserByUsername(ctx, payload.Username)
+		if err != nil {
+			return nil, err
+		}
+
+		err = bcrypt.CompareHashAndPassword([]byte(users.Password), []byte(payload.Password))
+		if err != nil {
+			return nil, common.ErrUnauthorized
+		}
+	}
+
+	accesstoken, err := b.jwt.GenerateAuthorizartionCode(entity.Claim{
+		UserID:   users.ID,
+		Username: users.Username,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	refreshToken := strings.ReplaceAll(uuid.NewString(), "-", "")
+
+	return &presentations.AuthorizationResp{
+		AccessToken:  accesstoken,
+		RefreshToken: refreshToken,
+	}, nil
+
+}
--- internal/business/business.go
+++ internal/business/business.go
@@ -1,13 +1,16 @@
 package business
 
 import (
-	"payslips/internal/provider"
+	"payslips/internal/business/auth"
 	"payslips/internal/repositories"
 )
 
 type Business struct {
+	Auth auth.Contract
 }
 
-func NewBusiness(repo *repositories.Repository, provider provider.Provider) Business {
-	return Business{}
+func NewBusiness(repo *repositories.Repository) Business {
+	return Business{
+		Auth: auth.NewBusiness(repo),
+	}
 }
