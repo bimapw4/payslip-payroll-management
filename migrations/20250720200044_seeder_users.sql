e51672e9d82d5f12f330f64a1423b890f9a14cca
bimapw4 <bimapw4@gmail.com>
2025-07-21 00:03:31 +0700

    [users] seed migrations - update seed

--- migrations/20250720200044_seeder_users.sql
+++ migrations/20250720200044_seeder_users.sql
@@ -1,9 +1,8 @@
 -- +goose Up
 -- +goose StatementBegin
--- Seeder untuk admin
 INSERT INTO users (id, name, username, password, salary, is_admin, is_active, created_at, updated_at, created_by, updated_by)
 VALUES
-(gen_random_uuid(), 'Admin User', 'admin', '$2a$10$7P2Xq7l2Hu.VnBf0M6HguuMnyYmEwJXKIXlpiQuSRcku4J47oGhI2', 0, TRUE, TRUE, now(), now(), 'system', '');
+(gen_random_uuid(), 'Admin User', 'admin', '$2a$10$01CFJTdZbxF7UjD45C31Ne2O4Gt11CmIBBuA8mREIpbcUrDT58TfK', 0, TRUE, TRUE, now(), now(), 'system', '');
 
 DO $$
 BEGIN
@@ -13,8 +12,8 @@ BEGIN
       gen_random_uuid(),
       CONCAT('Employee ', i),
       CONCAT('employee', i),
-      '$2a$10$7P2Xq7l2Hu.VnBf0M6HguuMnyYmEwJXKIXlpiQuSRcku4J47oGhI2',
-      (random() * 5000000 + 3000000)::INT, -- gaji random antara 3jt - 8jt
+      '$2a$10$01CFJTdZbxF7UjD45C31Ne2O4Gt11CmIBBuA8mREIpbcUrDT58TfK',
+      (random() * 5000000 + 3000000)::INT,
       FALSE,
       TRUE,
       now(),
