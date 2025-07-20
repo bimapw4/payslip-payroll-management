634d4cdaff3a140c5fb745633fb02653c0e001d9
bimapw4 <bimapw4@gmail.com>
2025-07-21 00:50:20 +0700

    [payroll] routes - register handler to routes payroll

---
+++ internal/routes/payroll.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"payslips/internal/handlers"
+	"payslips/internal/middleware"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func PayrollRouter(app *fiber.App, handler handlers.Handlers, m *middleware.Authentication) {
+	app.Post("/payroll", m.Authentication, handler.Payroll.Create)
+	app.Put("/payroll/running/:id", m.Authentication, handler.Payroll.Running)
+}
--- internal/routes/router.go
+++ internal/routes/router.go
@@ -12,6 +12,7 @@ func Routes(app *fiber.App, handler handlers.Handlers, m *middleware.Authenticat
 	routes := []func(app *fiber.App, handler handlers.Handlers, m *middleware.Authentication){
 		AuthRouter,
 		AttendanceRouter,
+		PayrollRouter,
 	}
 
 	for _, route := range routes {
