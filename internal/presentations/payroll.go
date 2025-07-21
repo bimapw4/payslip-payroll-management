710623289db1209a71b394b9c2113bf6b07d551e
bimapw4 <bimapw4@gmail.com>
2025-07-21 20:27:16 +0700

    [payslip] presentation - add response payslip

--- internal/presentations/payroll.go
+++ internal/presentations/payroll.go
@@ -12,3 +12,29 @@ type Payroll struct {
 	CreatedBy   string    `db:"created_by" json:"created_by"`
 	UpdatedBy   string    `db:"updated_by" json:"updated_by"`
 }
+
+type PayslipResponse struct {
+	PayrollID      string              `json:"payroll_id"`
+	Period         Period              `json:"period"`
+	Attendance     AttendanceBreakdown `json:"attendance"`
+	Overtime       OvertimeBreakdown   `json:"overtime"`
+	Reimbursements []Reimbursement     `json:"reimbursements"`
+	TotalTakeHome  int                 `json:"total_take_home"`
+}
+
+type Period struct {
+	Start time.Time `json:"start"`
+	End   time.Time `json:"end"`
+}
+
+type AttendanceBreakdown struct {
+	WorkingDays    int `json:"working_days"`
+	PresentDays    int `json:"present_days"`
+	AbsentDays     int `json:"absent_days"`
+	ProratedSalary int `json:"prorated_salary"`
+}
+
+type OvertimeBreakdown struct {
+	TotalHours  float64 `json:"total_hours"`
+	OvertimePay int     `json:"overtime_pay"`
+}
