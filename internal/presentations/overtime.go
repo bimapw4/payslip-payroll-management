d0bfcad89b27879ad1d58fb8087bb24ad60166e5
bimapw4 <bimapw4@gmail.com>
2025-07-21 10:51:43 +0700

    [overtime] presentations - add presentations overtime

---
+++ internal/presentations/overtime.go
@@ -0,0 +1,24 @@
+package presentations
+
+import (
+	"payslips/internal/common"
+	"time"
+)
+
+const (
+	ErrOvertimeNotExist     = common.Error("err overtime not exist")
+	ErrOvertimeAlreadyExist = common.Error("err overtime already exist")
+)
+
+type Overtime struct {
+	ID        string    `db:"id" json:"id"`
+	UserID    string    `db:"user_id" json:"user_id"`
+	StartTime time.Time `db:"start_time" json:"start_time"`
+	EndTime   time.Time `db:"end_time" json:"end_time"`
+	Duration  float64   `db:"duration" json:"duration"`
+	PayrollID *string   `db:"payroll_id" json:"payroll_id,omitempty"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+	CreatedBy string    `db:"created_by" json:"created_by"`
+	UpdatedBy string    `db:"updated_by" json:"updated_by"`
+}
